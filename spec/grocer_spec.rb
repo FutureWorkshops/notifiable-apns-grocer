e33ab974a684b5768ecea0f3dbc2bd9c2257bde7
Matt Brooke-Smith <matt@futureworkshops.com>
2014-01-16 21:49:14 +0000

    Update grocer spec

--- spec/grocer_spec.rb
+++ spec/grocer_spec.rb
@@ -5,7 +5,10 @@ describe Notifiable::Apns::Grocer::Stream do
   it "sends a single grocer notification" do    
             
     g = Notifiable::Apns::Grocer::Stream.new
-    g.send_notification(Notifiable::Notification.new("Test message", nil), Notifiable::DeviceToken.new("ABC123"))
+    n = Notifiable::Notification.create(:message => "Test message")
+    d = Notifiable::DeviceToken.create(:token => "ABC123")
+    
+    g.send_notification(n, d)
     
     Timeout.timeout(2) {
       notification = @grocer.notifications.pop
@@ -13,4 +16,28 @@ describe Notifiable::Apns::Grocer::Stream do
     }
   end
   
+  it "sends a single grocer notification in a batch" do    
+            
+    g = Notifiable::Apns::Grocer::Stream.new
+    n = Notifiable::Notification.create(:message => "Test message")
+    d = Notifiable::DeviceToken.create(:token => "ABC123", :provider => :apns)
+    u = User.new(d)
+    
+    Notifiable.batch do |b|
+      b.add(n, u)
+    end
+    Notifiable::NotificationDeviceToken.count.should == 1
+    
+    Timeout.timeout(2) {
+      notification = @grocer.notifications.pop
+      notification.alert.should eql "Test message"
+    }
+  end 
+  
+end
+
+User = Struct.new(:device_token) do
+  def device_tokens
+    [device_token]
+  end
 end
