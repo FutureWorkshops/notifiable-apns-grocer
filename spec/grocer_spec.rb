7e22720aa4feaa3a29274a3d53018221a02df273
Matt Brooke-Smith <matt@futureworkshops.com>
2014-03-17 01:02:21 +0000

    Update spec

--- spec/grocer_spec.rb
+++ spec/grocer_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
 describe Notifiable::Apns::Grocer::Stream do
-  
+
+  let(:a) { Notifiable::App.create }  
   let(:g) { Notifiable::Apns::Grocer::Stream.new }
-  let(:n) { Notifiable::Notification.create(:message => "Test message") }
-  let(:d) { Notifiable::DeviceToken.create(:token => "ABC123", :provider => :apns) }
+  let(:n) { Notifiable::Notification.create(:message => "Test message", :app => a) }
+  let(:d) { Notifiable::DeviceToken.create(:token => "ABC123", :provider => :apns, :app => a) }
   let(:u) { User.new(d) }
   
   it "sends a single grocer notification" do    
@@ -14,13 +15,14 @@ describe Notifiable::Apns::Grocer::Stream do
     Timeout.timeout(2) {
       notification = @grocer.notifications.pop
       notification.alert.should eql "Test message"
+      notification.custom[:notification_id].should == n.id
     }
   end
   
   it "sends a single grocer notification in a batch" do
     
-    Notifiable.batch do |b|
-      b.add(n, u)
+    Notifiable.batch(a) do |b|
+      b.add_notifiable(n, u)
     end
     Notifiable::NotificationStatus.count.should == 1
     Notifiable::NotificationStatus.first.status.should == 0
@@ -28,20 +30,22 @@ describe Notifiable::Apns::Grocer::Stream do
     Timeout.timeout(2) {
       notification = @grocer.notifications.pop
       notification.alert.should eql "Test message"
+      notification.custom[:notification_id].should == n.id
     }
   end 
   
   it "supports custom properties" do
     n.params = {:flag => true}
     
-    Notifiable.batch do |b|
-      b.add(n, u)
+    Notifiable.batch(a) do |b|
+      b.add_notifiable(n, u)
     end
     Notifiable::NotificationStatus.count.should == 1
     Notifiable::NotificationStatus.first.status.should == 0
     
     Timeout.timeout(2) {
       notification = @grocer.notifications.pop
+      notification.custom[:notification_id].should == n.id
       notification.custom[:flag].should == true
     }
   end
