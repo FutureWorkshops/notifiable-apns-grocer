81f8be435bf09af777519415a433f1e1cd3d376b
Matt Brooke-Smith <matt@futureworkshops.com>
2014-05-09 15:57:00 +0100

    Revert "Merge branch 'refs/heads/notifiable_0_18_0' into dev"
    This reverts commit b28f906e16ac770ab7b24744e2abc39da8e488f3, reversing
    changes made to d2f8e45da283988aed28218a04e3dbb460f1a266.

--- lib/notifiable/apns/grocer/stream.rb
+++ lib/notifiable/apns/grocer/stream.rb
@@ -75,8 +75,12 @@ module Notifiable
       
           def process_feedback
     				grocer_feedback.each do |attempt|
-    					device_token = DeviceToken.find_by_token(attempt.device_token)
-    					device_token.destroy if device_token
+    					token = attempt.device_token
+    					device_token = DeviceToken.find_by_token(token)
+    					if device_token
+    						device_token.update_attribute("is_valid", false) if device_token.updated_at < attempt.timestamp
+    						Rails.logger.info("Device #{token} (#{device_token.user_id}) failed at #{attempt.timestamp}")
+    					end
     				end
           end
   		end
