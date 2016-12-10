5f542c05a7837a0feccdefd4bd278635fb238d51
Matt Brooke-Smith <matt@futureworkshops.com>
2016-12-10 21:36:36 +0000

    Add ssl connection override

--- lib/notifiable/apns/grocer.rb
+++ lib/notifiable/apns/grocer.rb
@@ -1,6 +1,7 @@
 require 'notifiable'
-require "notifiable/apns/grocer/version"
-require "notifiable/apns/grocer/stream"
+require 'notifiable/apns/grocer/version'
+require 'notifiable/apns/grocer/stream'
+require 'notifiable/apns/grocer/grocer_ssl_connection'
 
 module Notifiable
   module Apns
